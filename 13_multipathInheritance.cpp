e3b64e92d85b4992dea782c8b0953f26ab88b366
subham <dark.demon.das@gmail.com>
2023-11-26 17:56:17 +0530

    no code like other forms of inheritance but have notes about this form of inheritance, the multipath inheritance

---
+++ 13_multipathInheritance.cpp
@@ -0,0 +1,10 @@
+/*
+    -> The form of inheritance which derives a new class by multiple inheritance of base classes, which are derived earlier from the same base class, is known as multipath inheritance.
+    -> It involves more than one form of inheritance, namely multilevel, multiple and hierarchical inheritances at one go.
+
+    -> For example, 
+        - let there be one "super class".
+        - we then inherited two more classes from that super class, which fulfil the example of "Hierarchical Inherirtance", namely "Middle classes".
+        - After that, we made another class which is the derivation of those two middle classes, fulfilling the example of "Multiple Inheritance", namely "Child Class".
+        - Also, we know that, the "Child class" is ultimetely derived from "middle class", which is also derived from the "Super class". So it also fulfill the example of "Multilevel Inheritance".
+*/
