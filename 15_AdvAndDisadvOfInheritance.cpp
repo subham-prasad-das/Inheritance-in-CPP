8da8d17f092312302709efc416833e692e0f5901
subham <dark.demon.das@gmail.com>
2023-11-26 18:00:35 +0530

    llighted out the advantages and disadvantages of inheritance

---
+++ 15_AdvAndDisadvOfInheritance.cpp
@@ -0,0 +1,17 @@
+/*
+    Folloeing are the Advantages of Inheritance:-
+        - Code resuability: here we can reuse our existing code to form a new one and avoid rewritting the same features again and again.
+        - Organization: here we can organize the code in a structural and logical way.
+        - Polymorphism: here we can create objects that can take on different forms, depending on the class they inherit from. This is called polymorphism, and it can make your code more flexible and adaptable to changing requirements.
+        - Overriding: here we can override methods in a derived class, which means one can change the way a method works without changing the original code. This is a powerful way to customize existing code to suit your needs.
+        - Abstraction: here one can create abstract classes that define the basic behavior of a group of related classes, while leaving the implementation details to the derived classes. This is called abstraction, and it can make your code more modular and easier to understand.: 
+
+    
+    Folloeing are the Disadvantages of Inheritance:-
+        
+        - Complexity – Inheritance in C++ can make your code more complex, especially when you have multiple levels of inheritance or when you’re inheriting from classes that have a lot of methods and properties. This can make it harder to understand how your code works and can lead to bugs and other issues.
+        - Rigidity – Inheritance in C++ can make your code more rigid, as it can be difficult to change the way a class works once it has been inherited by other classes. This can make it hard to adapt your code to changing requirements and can limit its overall flexibility.
+        - Dependency – Inheritance in C++ can create dependencies between classes, which can make it harder to reuse your code in other programs or to make changes to your code without affecting other parts of your program.
+        - Performance – Inheritance in C++ can impact the performance of your code, as it requires additional memory and processing power to maintain the relationship between classes. This can slow down your program and make it less efficient.
+        - Violation of encapsulation – Inheritance allows the child class to inherit the parent class’s protected and public members, but not the private members. However, if the child class overrides or accesses the parent class’s protected members, it can potentially lead to unexpected changes, errors, and violate the encapsulation principle. This can result in bugs and make the code harder to maintain.
+*/
