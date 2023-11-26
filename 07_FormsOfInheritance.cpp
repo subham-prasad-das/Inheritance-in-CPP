06cf112d43878f5c0c481e7d0cd7cb4a581465f2
subham <dark.demon.das@gmail.com>
2023-11-26 17:50:14 +0530

    discussed about the different forms of inheritance

---
+++ 07_FormsOfInheritance.cpp
@@ -0,0 +1,9 @@
+/*
+    -> Inheritace is classified into the following based on the interrelation among the classes involved in the inheritence process:-
+        - Single Inheritence : Derivation of a class from only one vase class.
+        - Multiple Inheritance : Derivation of a class from mote than one case classes.
+        - Multilevel Inheritance : Derivation of a class from a class which is derieved from oonother class.
+        - Hybrid Inheritance : Derivation of a class involving more than one form of inheritance.
+        - Hierarchial Inheritance : Derivation of more classes from a single class.
+        - Multipath Inheritance : Derivation of a class from other derived classes which are derived from a single bse class.
+*/
